96db8f337bfa51832fd1edef3cae094009fa93be
ts <tristansetha@gmail.com>
2021-03-12 08:57:46 -0800

    chrome boiler plate

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome.tabs.onActivated.addListener((tab) => {
+  chrome.tabs.get(tab.tabId, (current_tab_info) => {
+    // console.log(current_tab_info.url)
+    if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
+      chrome.tabs.executeScript(null, { file: "./foreground.js" }, () =>
+        console.log("i injected")
+      );
+    }
+  });
+});
+
+// chrome.tabs.executeScript(null, {file: './foreground.js'}, () => console.log('i injected'))
