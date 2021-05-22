6e050ef6e030c69c4598003d5bd2f2bce3780023
ts <tristansetha@gmail.com>
2021-05-21 18:17:26 -0700

    update

--- content.js
+++ content.js
@@ -1,23 +1,23 @@
 // document.querySelector('.lnXdpd').classList.add('spinspinspin');
+console.log("you'r in the world of content.js");
 
-// chrome.tabs.addListener(tab => {
-//   chrome.tabs.get(tab.tabId, current_tab_info => {
-//     console.log(current_tab_info.url);
-//   })
-// })
+// let blockedHtmlUrl = chrome.extension.getURL("blocked.html");
+// let currentUrl = "";
 
-chrome.storage.local.get('colour', (response) => {
-  if (response.colour) {
-    document.body.style.backgroundColor = response.colour;
-  }
-});
+// chrome.storage.local.get("currentUrl", (response) => {
+//   if (response.currentUrl) {
+//     currentUrl = response.currentUrl;
+//   }
+// });
 
-
-chrome.runtime.onMessage.addListener(request => {
-  console.log(request);
-  console.log(chrome.extension.getURL("blocked.html"));
-
-  if (request.colour) {
-    document.body.style.backgroundColor = request.colour;
-  }
-});
+// chrome.runtime.onMessage.addListener((request) => {
+//   console.log(chrome.extension.getURL("blocked.html"));
+//   chrome.tabs.addListener((tab) => {
+//     chrome.tabs.get(tab.tabId, (current_tab_info) => {
+//       console.log(current_tab_info.url);
+//       if (current_tab_info.url === currentUrl) {
+//         window.location.href = blockedHtmlUrl;
+//       }
+//     });
+//   });
+// });
--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 const focusButton = document.querySelector("#focus-btn");
 const regex = new RegExp("https://www.youtube.com/watch*");
+let currentUrl = ""
+let focusUrl
 
 chrome.tabs.query({ active: true }, (tabs) => {
   console.log(tabs);
@@ -7,19 +9,21 @@ chrome.tabs.query({ active: true }, (tabs) => {
       focusButton.disabled = true;
     } else {
       focusButton.disabled = false;
+      currentUrl = tabs[0].url;
     }
 });
-
+//
 document.querySelector("#focus-btn").addEventListener("click", () => {
   console.log("clicked");
   // read the colour that the user has selected
-  const colour = document.querySelector("#colour-input").value;
+  // const colour = document.querySelector("#colour-input").value;
 
   // get all the google tabs and send a message to their tabs
-  console.log(colour);
+  // console.log(colour);
   chrome.tabs.query({ url: "https://*.youtube.com/*" }, (tabs) => {
+    focusUrl = currentUrl
     tabs.forEach((tab) => {
-      chrome.tabs.sendMessage(tab.id, { colour });
+      chrome.tabs.sendMessage(tab.id, { focusUrl });
     });
   });
 });
