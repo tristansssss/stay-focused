5315b829adb6d71609eca016fa5b214f030f2f14
ts <tristansetha@gmail.com>
2021-05-24 23:41:07 -0700

    update

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 chrome.tabs.onUpdated.addListener((tab) => {
   chrome.tabs.query({ url: "https://*.youtube.com/*" }, (tabs) => {
     chrome.tabs.insertCSS(null, { file: "./content.css" });
-    console.log("insertign css")
     tabs.forEach((tab) => {
       let tabUrl = tab.url;
       chrome.tabs.sendMessage(tab.id, { tabUrl });
